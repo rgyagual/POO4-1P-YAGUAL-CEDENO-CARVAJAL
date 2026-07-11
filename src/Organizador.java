fb41f0be580f3a066baef3ad03202588bd848036
rgyagual <g300607y@gmail.com>
2026-07-11 17:27:53 -0500

    Implementar Herencia, Actualizar constructor y añadir base de método abstracto

--- src/Organizador.java
+++ src/Organizador.java
@@ -1,26 +1,35 @@
-public class Organizador {
+public class Organizador extends Usuario {
     private String empresa;
     private String cargo;
 
-    public Organizador(String empresa, String cargo){
-        this.empresa=empresa;
-        this.cargo=cargo;
+    public Organizador(String codigoUnico, String cedula,
+            String nombres, String apellidos,
+            String usuario, String contraseña, String correo,
+            Rol rol, String empresa, String cargo) {
+
+        super(codigoUnico, cedula, nombres,
+                apellidos, usuario, contraseña,
+                correo, rol);
+        this.empresa = empresa;
+        this.cargo = cargo;
     }
 
-    public String getEmpresa(){
+    public String getEmpresa() {
         return this.empresa;
     }
 
-    public void setEmpresa(String empresa){
-        this.empresa=empresa;
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
     }
 
-    public String getCargo(){
+    public String getCargo() {
         return this.cargo;
     }
-    
-    public void setCargo(String cargo){
-        this.cargo=cargo;
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
     }
-    
+ 
+    @Override
+    public void consultarEntradas(){}
 }
