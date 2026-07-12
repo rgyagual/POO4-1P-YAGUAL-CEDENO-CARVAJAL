c44f446e5afda202054c5cc03e9db087cf2f916a
rgyagual <g300607y@gmail.com>
2026-07-11 22:22:06 -0500

    Implementar métodos para leer Archivos .txt y cargar datos

--- src/Sistema.java
+++ src/Sistema.java
@@ -1,3 +1,6 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -58,6 +61,152 @@ public class Sistema {
 
     public void setCompras(ArrayList<Compra> compras) {
         this.compras = compras;
+    }
+
+    // ================================
+    // Implementar clase ManejoArchivos para leer los.txt
+    // ======================================
+    public void cargarUsuarios() {
+        ArrayList<String> list_usuarios = ManejoArchivos.LeeFichero("usuarios.txt");
+        ArrayList<String> list_aficionados = ManejoArchivos.LeeFichero("aficionados.txt");
+        ArrayList<String> list_organizadores = ManejoArchivos.LeeFichero("organizadores.txt");
+        // Obtención de datos usuario
+        for (String dato : list_usuarios) {
+            String[] dato_usuario = dato.split("\\|");
+            String codigoUnico = dato_usuario[0];
+            String cedula = dato_usuario[1];
+            String nombre = dato_usuario[2];
+            String apellido = dato_usuario[3];
+            String usuario = dato_usuario[4];
+            String contrasena = dato_usuario[5];
+            String correo = dato_usuario[6];
+            String rol = dato_usuario[7];
+
+            // Crear usuario Aficionado
+            if (rol.equals("A")) {
+                for (String linea : list_aficionados) {
+                    String[] datos_aficionados = linea.split("\\|");
+                    // datos aficionado
+                    String celular = datos_aficionados[4];
+                    String paisFavorito = datos_aficionados[5];
+                    if (datos_aficionados[0].equals(codigoUnico)) {
+                        Aficionado a = new Aficionado(
+                                codigoUnico,
+                                cedula,
+                                nombre,
+                                apellido,
+                                usuario,
+                                contrasena,
+                                correo,
+                                celular,
+                                paisFavorito);
+                        usuarios.add(a);
+                        break;
+                    }
+                }
+                // crear usuario Organizador
+            } else if (rol.equals("O")) {
+                for (String linea : list_organizadores) {
+                    String[] datos_organizador = linea.split("\\|");
+                    // datos organizador
+                    String empresa = datos_organizador[4];
+                    String cargo = datos_organizador[5];
+                    if (codigoUnico.equals(datos_organizador[0])) {
+                        Organizador o = new Organizador(
+                                codigoUnico,
+                                cedula,
+                                nombre,
+                                apellido,
+                                usuario,
+                                contrasena,
+                                correo,
+                                empresa,
+                                cargo);
+                        usuarios.add(o);
+                        break;
+                    }
+                }
+            }
+
+        }
+
+    }
+
+    public void cargarPartidos() throws ParseException {
+        SimpleDateFormat formatoFecha = new SimpleDateFormat("yyyy-MM-dd");
+        ArrayList<String> list_partidos = ManejoArchivos.LeeFichero("partidos.txt");
+        for (String datos : list_partidos) {
+            String[] datos_partido = datos.split("\\|");
+            // Obtencion datos de partido
+            String codigo = datos_partido[0];
+            String equipoLocal = datos_partido[1];
+            String equipoVisitante = datos_partido[2];
+            Date fecha = formatoFecha.parse(datos_partido[3]);
+            String estadio = datos_partido[4];
+            String ciudad = datos_partido[5];
+            int capacidad = Integer.parseInt(datos_partido[6]);
+            int capacidadGeneral = Integer.parseInt(datos_partido[7]);
+            int capacidadPreferencial = Integer.parseInt(datos_partido[8]);
+            int capacidadVip = Integer.parseInt(datos_partido[9]);
+            String fase = datos_partido[10];
+
+            Partido p = new Partido(
+                    codigo,
+                    equipoLocal,
+                    equipoVisitante,
+                    fecha,
+                    estadio,
+                    ciudad,
+                    fase,
+                    capacidad,
+                    capacidadGeneral,
+                    capacidadPreferencial,
+                    capacidadVip,
+                    0.0,
+                    0.0,
+                    0.0);
+            partidos.add(p);
+        }
+
+    }
+
+    public void cargarKits() {
+        ArrayList<String> list_kits = new ArrayList<>();
+        // Obtencion Datos Kits
+        for (String datos : list_kits) {
+            String[] datos_kits = datos.split("\\|");
+            String codigo = datos_kits[0];
+            String nombre = datos_kits[1];
+            String descripcion = datos_kits[2];
+            Double precio = Double.parseDouble(datos_kits[4]);
+            int kitsDisponibles = Integer.parseInt(datos_kits[5]);
+
+            /**
+             * Se crea un ArrayList para almacenar los partidos que tendrá un Kit
+             * 
+             */
+            ArrayList<Partido> partidosKits = new ArrayList<>();
+
+            // Obtención de los partidos del Kit
+            for (String c : datos_kits[2].split(",")) {
+                for (Partido p : partidos) {
+                    if (p.getCodigoPartido().equals(c)) {
+                        partidosKits.add(p);
+                    }
+                }
+
+            }
+            Kit k = new Kit(
+                    codigo,
+                    nombre,
+                    descripcion,
+                    precio,
+                    kitsDisponibles,
+                    partidosKits);
+
+            kits.add(k);
+        }
+
     }
 
     /**
@@ -166,7 +315,8 @@ public class Sistema {
                     }
                 }
             }
-        } if(!autenticado){
+        }
+        if (!autenticado) {
             System.out.println("Usuario o contraseña incorrectos.");
         }
     }
