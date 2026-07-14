002e04b00beb00bd9dd2b380eca58389f789d3d1
Daniela-Cedeno <danlucio@espol.edu.ec>
2026-07-14 10:55:02 -0500

    Documentar código y generar JavaDoc

--- src/Partido.java
+++ src/Partido.java
@@ -1,27 +1,62 @@
 import java.util.Date;
 
+/**
+ * Representa 
+ * Partido
+ */
 public class Partido {
-
+    // ====================================
+    // ATRIBUTOS
+    // ====================================
+    /** Código del partido. */
     private String codigoPartido;
+    /** Nombre del equipo local. */
     private String equipoLocal;
+    /** Nombre del equipo visitante. */
     private String equipoVisitante;
+    /** Fecha del partido. */
     private Date fechaPartido;
+    /** Nombre del estadio. */
     private String estadio;
+    /** Nombre de la ciudad. */
     private String ciudad;
+    /** Fase del partido. */
     private String faseEnum;
+    /** Fase del partido en Enum. */
     private FasesMundial fase;
+    /** Capacidad del estadio. */
     private int capacidad;
+    /** Entradas disponibles en general. */
     private int entradaGeneral;
+    /** Entradas disponibles en preferencial. */
     private int entradaPreferencial;
+    /** Entradas disponibles en VIP. */
     private int entradaVip;
 
+    // ====================
+    // CONSTRUCTOR
+    // ====================
+    /**
+     * Crea un nuevo partido.
+     * 
+     * @param codigoPartido        Código del partido.
+     * @param equipoLocal          Nombre del equipo local.
+     * @param equipoVisitante      Nombre del equipo visitante.
+     * @param fechaPartido         Fecha del partido.
+     * @param estadio              Nombre del estadio.
+     * @param ciudad               Nombre de la ciudad.
+     * @param faseEnum             Fase del partido
+     * @param capacidad            Capacidad del estadio.
+     * @param entradaGeneral       Entradas disponibles en general.
+     * @param entradaPreferencial  Entradas disponibles en preferencial.
+     * @param entradaVip           Entradas disponibles en Vip.
+     */
     public Partido(String codigoPartido, String equipoLocal,
             String equipoVisitante, Date fechaPartido,
             String estadio, String ciudad, 
             String faseEnum, int capacidad,
             int entradaGeneral, int entradaPreferencial,
-            int entradaVip, double precioGeneral,
-            double precioPreferencial, double precioVip) {
+            int entradaVip) {
 
         this.codigoPartido = codigoPartido;
         this.equipoLocal = equipoLocal;
@@ -56,6 +91,9 @@ public class Partido {
         }
     }
 
+    // ===================================
+    // Getters y Setters
+    // ===================================
     public String getCodigoPartido() {
         return codigoPartido;
     }
@@ -172,6 +210,11 @@ public class Partido {
         this.entradaVip = entradaVip;
     }
 
+    /**
+     * Devuelve una representación en texto con la información del partido.
+     * 
+     * @return Cadena con datos del partido.
+     */
     @Override
     public String toString() {
         String partido = equipoLocal + " vs " + equipoVisitante;
